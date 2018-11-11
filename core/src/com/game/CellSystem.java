80d1182b1268b1cabc38f6f1c490f94d55cf23c4
Javier <takatsho@gmail.com>
2018-11-11 22:10:03 +0100

    All CellSystem tests done.

--- core/src/com/game/CellSystem.java
+++ core/src/com/game/CellSystem.java
@@ -12,8 +12,8 @@ public class CellSystem {
         this.width = width;
         this.height = height;
         grid = new Cell[width][height];
-        for (int i = 0; i < width; i++){
-            for (int j = 0; j < height; j++){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 grid[i][j] = new Cell();
             }
         }
@@ -24,6 +24,26 @@ public class CellSystem {
     }
 
     public boolean isUnderpopulated(int x, int y) {
+        int neighborsAlive = getNeighborsAlive(x, y);
+        return neighborsAlive < 2;
+    }
+
+    public boolean isInStasis(int x, int y) {
+        int neighboursAlive = getNeighborsAlive(x, y);
+        return neighboursAlive == 2 || neighboursAlive == 3;
+    }
+
+    public boolean isOverpopulated(int x, int y) {
+        int neighboursAlive = getNeighborsAlive(x, y);
+        return neighboursAlive > 3;
+    }
+
+    public boolean canReproduce(int x, int y) {
+        int neighboursAlive = getNeighborsAlive(x, y);
+        return neighboursAlive == 3;
+    }
+
+    private int getNeighborsAlive(int x, int y) {
         int neighborsAlive = 0;
         if (getCell(x - 1, y - 1).isAlive()) {
             neighborsAlive += 1;
@@ -49,7 +69,6 @@ public class CellSystem {
         if (getCell(x + 1, y + 1).isAlive()) {
             neighborsAlive += 1;
         }
-        System.out.println(neighborsAlive);
-        return neighborsAlive < 2;
+        return neighborsAlive;
     }
 }
--- core/src/com/game/CellSystemTest.java
+++ core/src/com/game/CellSystemTest.java
@@ -2,26 +2,59 @@ package com.game;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CellSystemTest {
+    private CellSystem setUpSystemWithNeighbours(int numberOfNeighbours) {
+        CellSystem cellSystem = new CellSystem(3, 3);
+
+        if (numberOfNeighbours == 0) {
+            cellSystem.getCell(1, 1).revive();
+        } else if (numberOfNeighbours == 1) {
+            cellSystem.getCell(1, 1).revive();
+            cellSystem.getCell(2, 1).revive();
+
+        } else if (numberOfNeighbours == 2) {
+            cellSystem.getCell(1, 1).revive();
+            cellSystem.getCell(0, 1).revive();
+            cellSystem.getCell(2, 1).revive();
+        } else if (numberOfNeighbours == 3) {
+            cellSystem.getCell(1, 1).revive();
+            cellSystem.getCell(0, 1).revive();
+            cellSystem.getCell(0, 2).revive();
+            cellSystem.getCell(1, 0).revive();
+        } else if (numberOfNeighbours == 4) {
+            cellSystem.getCell(1, 1).revive();
+            cellSystem.getCell(0, 1).revive();
+            cellSystem.getCell(0, 2).revive();
+            cellSystem.getCell(2, 1).revive();
+            cellSystem.getCell(1, 0).revive();
+        } else if (numberOfNeighbours == 5) {
+            cellSystem.getCell(1, 1).revive();
+            cellSystem.getCell(0, 1).revive();
+            cellSystem.getCell(0, 2).revive();
+            cellSystem.getCell(1, 0).revive();
+            cellSystem.getCell(1, 2).revive();
+            cellSystem.getCell(2, 2).revive();
+        }
+        return cellSystem;
+    }
+
     @Test
     public void testCheckCellIsUnderpopulatedWithZeroNeighbours() {
         //Given
-        CellSystem cellSystem = new CellSystem(3,3);
+        CellSystem zeroNeighbourSystem = setUpSystemWithNeighbours(0);
         //When
-        cellSystem.getCell(1, 1).revive();
         //Then
-        assertTrue(cellSystem.isUnderpopulated(1, 1));
+        assertTrue(zeroNeighbourSystem.isUnderpopulated(1, 1));
     }
 
     @Test
     public void testCheckCellIsUnderpopulatedWithOneNeighbours() {
         //Given
-        CellSystem cellSystem = new CellSystem(3,3);
+        CellSystem cellSystem = setUpSystemWithNeighbours(1);
         //When
-        cellSystem.getCell(1, 1).revive();
-        cellSystem.getCell(2, 1).revive();
         //Then
         assertTrue(cellSystem.isUnderpopulated(1, 1));
     }
@@ -29,27 +62,97 @@ class CellSystemTest {
     @Test
     public void testCheckCellIsNotUnderpopulatedWithTwoNeighbours() {
         //Given
-        CellSystem cellSystem = new CellSystem(3,3);
+        CellSystem cellSystem = setUpSystemWithNeighbours(2);
         //When
-        cellSystem.getCell(1, 1).revive();
-        cellSystem.getCell(0, 1).revive();
-        cellSystem.getCell(2, 1).revive();
         //Then
         assertFalse(cellSystem.isUnderpopulated(1, 1));
     }
 
     @Test
-    public void testCheckCellIsInStasis() {
-
+    public void testCheckCellIsInStasisWithTwoNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(2);
+        //When
+        //Then
+        assertTrue(cellSystem.isInStasis(1, 1));
     }
 
     @Test
-    public void testCheckCellIsOverpopulated() {
-
+    public void testCheckCellIsInStasisWithThreeNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(3);
+        //When
+        //Then
+        assertTrue(cellSystem.isInStasis(1, 1));
     }
 
     @Test
-    public void testCheckCellCanReproduce() {
+    public void testCheckCellIsNotInStasisWithZeroNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(0);
+        //When
+        //Then
+        assertFalse(cellSystem.isInStasis(1, 1));
+    }
 
+    @Test
+    public void testCheckCellIsNotInStasisWithFourNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(4);
+        //Then
+        assertFalse(cellSystem.isInStasis(1, 1));
+    }
+
+    @Test
+    public void testCheckCellIsOverpopulatedWithFourNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(4);
+        //Then
+        assertTrue(cellSystem.isOverpopulated(1, 1));
+    }
+
+    @Test
+    public void testCheckCellIsNotOverpopulatedWithThreeNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(3);
+        //When
+        //Then
+        assertFalse(cellSystem.isOverpopulated(1, 1));
+    }
+
+    @Test
+    public void testCheckCellIsOverpopulatedWithFiveNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(5);
+        //When
+        //Then
+        assertTrue(cellSystem.isOverpopulated(1, 1));
+    }
+
+    @Test
+    public void testCheckCellCanReproduceWithThreeNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(3);
+        //When
+        //Then
+        assertTrue(cellSystem.canReproduce(1, 1));
+    }
+
+    @Test
+    public void testCheckCellCanNotReproduceWithTwoNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(2);
+        //When
+        //Then
+        assertFalse(cellSystem.canReproduce(1, 1));
+    }
+
+    @Test
+    public void testCheckCellCanNotReproduceWithFourNeighbours() {
+        //Given
+        CellSystem cellSystem = setUpSystemWithNeighbours(4);
+        //When
+        //Then
+        assertFalse(cellSystem.canReproduce(1, 1));
     }
 }
